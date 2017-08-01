a2859407564e8133aaec501bdaf9db8b18ae97ff
Dylan Malone Stuart <dyln.strt@gmail.com>
2017-08-01 08:41:46 +0100

    Added character recognition files (#7)
    * Added character recognition files
    
    * Fixed classes text file and scipy.misc import in notebook

---
+++ bnn/src/training/characters-gen-binary-weights.py
@@ -0,0 +1,53 @@
+#BSD 3-Clause License
+#=======
+#
+#Copyright (c) 2017, Xilinx
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+#* Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#* Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#* Neither the name of the copyright holder nor the names of its
+#  contributors may be used to endorse or promote products derived from
+#  this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import finnthesizer as fth
+
+if __name__ == "__main__":
+    bnnRoot = "."
+    npzFile = bnnRoot + "/char_parameters.npz"
+    targetDirBin = bnnRoot + "/nist"
+    
+    simdCounts = [64, 32, 64, 8]
+    peCounts = [32, 64, 32, 16]
+    
+    #classes = map(lambda x: str(x), range(61))
+    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", #Digits
+"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", #Upper case
+"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] #Lower case
+    
+    fth.convertFCNetwork(npzFile, targetDirBin, simdCounts, peCounts)
+    
+    with open(targetDirBin + "/classes.txt", "w") as f:
+        f.write("\n".join(classes))
+
