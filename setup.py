2fe7f6ad3968bcb4b496203b3e7e73e325d3b4a2
giuliogamba <giuliog@xilinx.com>
2018-08-09 11:00:43 +0100

    Added support to Pynq-Z2

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ import os
 from glob import glob
 import site 
 
-if 'BOARD' not in os.environ or os.environ['BOARD'] != 'Pynq-Z1':
-    print("Only supported on a Pynq Z1 Board")
+if 'BOARD' not in os.environ or not (os.environ['BOARD'] == 'Pynq-Z1' or os.environ['BOARD'] == 'Pynq-Z2'):
+    print("Only supported on a Pynq Z1 or Z2 boards")
     exit(1)
 
 
