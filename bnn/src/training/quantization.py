5921b07621e685672c138a4642a0fa7aa36e7e58
Nick Fraser <icanlosh@gmail.com>
2019-02-21 15:21:05 +0000

    Updated license information.

--- bnn/src/training/quantization.py
+++ bnn/src/training/quantization.py
@@ -4,6 +4,10 @@
 #Copyright (c) 2019, Xilinx Inc.
 #All rights reserved.
 #
+#Based on Matthieu Courbariaux's BinaryNet example
+#Copyright (c) 2015-2016, Matthieu Courbariaux
+#All rights reserved
+#
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
 #
